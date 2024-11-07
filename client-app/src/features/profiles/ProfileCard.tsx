fb34123587cebe00e578bc89b2c9c1ae621cbf41
Nickee <nickee.kang@gosaas.io>
2024-11-07 14:02:08 +0800

    implement follow feature

--- client-app/src/features/profiles/ProfileCard.tsx
+++ client-app/src/features/profiles/ProfileCard.tsx
@@ -1,6 +1,7 @@
 import { Profile } from "../../app/models/profile";
 import { Card, Icon, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import FollowButton from "./FollowButton";
 
 interface Props {
     profile: Profile;
@@ -16,8 +17,9 @@ export default function ProfileCard({profile}: Props){
             </Card.Content>
             <Card.Content extra>
                 <Icon name="user" />
-                20 followers
+                {profile.followersCount} followers
             </Card.Content>
+            <FollowButton profile={profile}/>
         </Card>
     )
 }
